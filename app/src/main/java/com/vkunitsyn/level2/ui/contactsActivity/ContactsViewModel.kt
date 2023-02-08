90a748fb8c147d686ef30edfdef08deb271088cb
Vit0920 <111330096+Vit0920@users.noreply.github.com>
2023-02-08 12:36:47 +0800

    Edited ViewModel

--- app/src/main/java/com/vkunitsyn/level2/ui/contactsActivity/ContactsViewModel.kt
+++ app/src/main/java/com/vkunitsyn/level2/ui/contactsActivity/ContactsViewModel.kt
@@ -1,5 +1,6 @@
 package com.vkunitsyn.level2.ui.contactsActivity
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.vkunitsyn.level2.model.Contact
@@ -7,31 +8,37 @@ import com.vkunitsyn.level2.utils.ContactsData
 
 class ContactsViewModel : ViewModel() {
 
+    private val _contactsList =  MutableLiveData<List<Contact>>()
+    val contactsList: LiveData<List<Contact>> = _contactsList
 
-
-    val contactsList: MutableLiveData<ArrayList<Contact>> by lazy {
-        MutableLiveData<ArrayList<Contact>>()
-    }
 
     init {
-        contactsList.value = ContactsData.getData()
+        _contactsList.value = ContactsData.getData()
     }
 
-    fun add(position: Int, contact: Contact ){
-        contactsList.value?.add(position,contact)
+    fun add(position: Int, contact: Contact){
+        val currentContactsList = _contactsList.value ?: emptyList()
+        val newContactsList = currentContactsList.toMutableList()
+        newContactsList.add(position,contact)
+        _contactsList.value = newContactsList
     }
 
     fun remove (contact: Contact){
-        contactsList.value?.remove(contact)
+        val currentContactsList = _contactsList.value ?: emptyList()
+        val newContactsList = currentContactsList.toMutableList()
+        newContactsList.remove(contact)
+        _contactsList.value = newContactsList
     }
 
     fun removeAt(position: Int){
-        contactsList.value?.removeAt(position)
+        val currentContactsList = _contactsList.value ?: emptyList()
+        val newContactsList = currentContactsList.toMutableList()
+        newContactsList.removeAt(position)
+        _contactsList.value = newContactsList
     }
 
-    fun get (position: Int): Contact? = contactsList.value?.get(position)
+    fun get (position: Int): Contact? = _contactsList.value?.get(position)
 
 
-    fun getSize(): Int? = contactsList.value?.size
-    }
-
+    fun getSize(): Int? = _contactsList.value?.size
+}
